aba98bb33b7fd0dfacbc0dea1c1d2ed01cf93674
vsorokolit <sorokolitvovan@gmail.com>
2023-11-06 14:12:52 +0200

    initial2

--- app/src/main/java/com/kotik/shppapplication/MyProfile.kt
+++ app/src/main/java/com/kotik/shppapplication/MyProfile.kt
@@ -3,9 +3,9 @@ package com.kotik.shppapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Login : AppCompatActivity() {
+class MyProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_my_profile)
     }
 }
